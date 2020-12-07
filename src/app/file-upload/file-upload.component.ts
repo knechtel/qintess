23513980d75ce08e6801f41d0ee3833fd8b2b667
maiquelknechtel <maiquelknechtel@gmail.com>
2020-12-07 16:03:00 -0300

    Refactor em file-upload nav-bar e cadastro-cidade.

--- src/app/file-upload/file-upload.component.ts
+++ src/app/file-upload/file-upload.component.ts
@@ -1,12 +1,13 @@
-import { HttpClient, HttpEventType } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
- 
+import { HttpHeaders } from '@angular/common/http';
 @Component({
   selector: 'app-file-upload',
   templateUrl: './file-upload.component.html',
   styleUrls: ['./file-upload.component.css']
 })
 export class FileUploadComponent implements OnInit {
+ 
   selectedFile!: File;
   retrievedImage: any;
   base64Data: any;
@@ -14,51 +15,43 @@ export class FileUploadComponent implements OnInit {
   message!: string;
   imageName: any;
 
+  httpOptions = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+  }
   constructor(private httpClient: HttpClient) { }
 
   ngOnInit(): void {
 
   }
-//Gets called when the user selects an image
-public onFileChanged(event:any) {
-  //Select File
-  this.selectedFile = event.target.files[0];
-}
+  public onFileChanged(event: any) {
+    this.selectedFile = event.target.files[0];
+  }
 
 
 
-onUpload() {
- 
-  
- 
-  const uploadImageData = new FormData();
-  uploadImageData.append('imageFile', this.selectedFile, this.selectedFile.name);
-
-
-  this.httpClient.post('http://localhost:8080/api/estado/upload/', uploadImageData, { observe: 'response' })
-    .subscribe((response) => {
-      if (response.status === 200) {
-        this.message = 'Image uploaded successfully';
-      } else {
-        this.message = 'Image not uploaded successfully';
+  onUpload() {
+    const uploadImageData = new FormData();
+    uploadImageData.append('imageFile', this.selectedFile, this.selectedFile.name);
+    this.httpClient.post('http://localhost:8080/api/estado/upload/', uploadImageData, { observe: 'response' })
+      .subscribe((response) => {
       }
-    }
-    );
+      );
 
 
-}
-
-  //Gets called when the user clicks on retieve image button to get the image from back end
+  }
   getImage() {
-  //Make a call to Sprinf Boot to get the Image Bytes.
-  this.httpClient.get('http://localhost:8080/api/estado/Request/' + this.imageName)
-    .subscribe(
-      res => {
-        this.retrieveResonse = res;
-        this.base64Data = this.retrieveResonse.picByte;
-        this.retrievedImage = 'data:image/jpeg;base64,' + this.base64Data;
-      }
-    );
-}
+    this.httpClient.get('http://localhost:8080/api/estado/Request/' + this.imageName)
+      .subscribe(
+        res => {
+          this.retrieveResonse = res;
+          this.base64Data = this.retrieveResonse.picByte;
+          this.retrievedImage = 'data:image/jpeg;base64,' + this.base64Data;
+        }
+      );
+  }
+
+  async doJob() {
+    await this.httpClient.post('http://localhost:8080/api/estado/import/',this.httpOptions).toPromise()
+  }
 
 }
